0c2cbb948d3db34f0a773e633c22c582b1a97c01
Matias <marazza.matias@gmail.com>
2024-06-13 03:44:09 -0300

    parse JSON

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -1,20 +1,21 @@
 import { PCD, SerializedPCD } from "@pcd/pcd-types";
 
-export const authenticate = async (multiPCDs: SerializedPCD<PCD<unknown, unknown>>[]) => {
+export const authenticate = async (
+  multiPCDs: SerializedPCD<PCD<unknown, unknown>>[]
+) => {
   try {
-      const response = await fetch(`/api/auth/authenticate`, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: multiPCDs as unknown as BodyInit
-      });
-
-      const data = await response.json();
-
-      return data;
+    const response = await fetch(`/api/auth/authenticate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(multiPCDs)
+    });
+    console.log(JSON.stringify(multiPCDs));
+    const data = await response.json();
+    return data;
   } catch (error) {
-      console.error('There was an error with the validation:', error);
-      return false;
+    console.error("There was an error with the validation:", error);
+    return false;
   }
 };
